4dfdc5f11b81372218b3a6ad045aa2c944d90a90
Dpbm <dpbm136@gmail.com>
2023-12-13 11:23:42 -0300

    updated target folder

--- datasets/get_kaggle.py
+++ datasets/get_kaggle.py
@@ -1,22 +1,36 @@
 import os
+import sys
 
-def get_dataset(dataset):
+DEFAULT_FOLDER = "./"
+
+def get_dataset(dataset, folder=DEFAULT_FOLDER):
     dataset_name = dataset.split('/')[-1]
-    dataset_path = f'./{dataset_name}'
+    dataset_path = f'{folder}{dataset_name}'
+
     if(not os.path.exists(dataset_path)):
         os.mkdir(dataset_path)
+    
     os.system(f"kaggle datasets download {dataset} --path {dataset_path} --unzip")
 
-def get_datasets(datasets_file):
+def get_datasets(datasets_file, folder=DEFAULT_FOLDER):
     print("---Getting data from kaggle---")    
+    
     with open(datasets_file, "r") as file:
         datasets = [dataset.strip() for dataset in file.readlines()]
         total = len(datasets)
 
         for i, dataset in enumerate(datasets):
             print(f"({i+1}/{total}) {dataset}")
-            get_dataset(dataset)
+            get_dataset(dataset, folder)
 
 if __name__ == '__main__':
-    kaggle_datasets = 'kaggle.txt'
-    get_datasets(kaggle_datasets)
+    arguments = sys.argv
+    total_arguments = len(arguments)
+    
+    if(total_arguments < 2 or total_arguments > 3):
+        print("Usage: python get_kaggle.py ./datasets.txt [/target_folder/]")
+        sys.exit(1)
+    
+    kaggle_datasets = arguments[1]
+    target_folder = arguments[-1]
+    get_datasets(kaggle_datasets, target_folder)

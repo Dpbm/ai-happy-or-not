f46f5677f243c428a4a1f161e84b4da93b0fb947
Dpbm <dpbm136@gmail.com>
2023-12-13 11:17:00 -0300

    updated get_kaggle script

--- datasets/get_kaggle.py
+++ datasets/get_kaggle.py
@@ -3,15 +3,16 @@ import os
 def get_dataset(dataset):
     os.system(f"kaggle datasets download {dataset} --unzip")
 
-if __name__ == '__main__':
-    print("---Getting data from kaggle---")
-    
-    kaggle_datasets = 'kaggle.txt'
-
-    with open(kaggle_datasets, "r") as file:
+def get_datasets(datasets_file):
+    print("---Getting data from kaggle---")    
+    with open(datasets_file, "r") as file:
         datasets = [dataset.strip() for dataset in file.readlines()]
         total = len(datasets)
 
         for i, dataset in enumerate(datasets):
             print(f"({i+1}/{total}) {dataset}")
             get_dataset(dataset)
+
+if __name__ == '__main__':
+    kaggle_datasets = 'kaggle.txt'
+    get_datasets(kaggle_datasets)
